45f08d012cf6d9587e555363af9e29acb51ce931
Alastair Porter <alastair.porter@upf.edu>
2013-10-03 14:03:17 +0200

    finish logger that carries extra info

--- compmusic/essentia/__init__.py
+++ compmusic/essentia/__init__.py
@@ -5,6 +5,7 @@ class EssentiaModule(object):
         classname = "%s.%s" % (self.__class__.__module__, self.__class__.__name__)
         self.logger = log.get_logger(classname, self.__version__)
 
-    def do_run(self, docid, sourcefile, fname):
-        self.logger.set_document(docid)
+    def process_document(self, docid, sourcefileid, fname):
+        self.logger.set_documentid(docid)
+        self.logger.set_sourcefileid(sourcefileid)
         self.run(fname)
--- compmusic/essentia/log.py
+++ compmusic/essentia/log.py
@@ -2,19 +2,30 @@ import logging
 
 essentia_log = logging.getLogger("essentia")
 
-# module/moduleversion
-# sourcefile
-# document
-
 class EssentiaAdapter(logging.LoggerAdapter):
+    """ A logging adapter that lets you set the document and sourcefile
+    being processed.
+    You can also pass in `modulename` and `moduleversion` as
+    the `extra` parameter (see 
+    http://docs.python.org/2/howto/logging-cookbook.html#using-loggeradapters-to-impart-contextual-information )
+    and it will send all of this information to the logger handler.
+    """
 
-    def set_document(self, doc):
-        self.doc = doc
+    def set_documentid(self, docid):
+        self.docid = docid
+
+    def set_sourcefileid(self, sfid):
+        self.sourcefileid = sfid
 
     def process(self, msg, kwargs):
-        if hasattr(self, "doc"):
-            kwargs["doc"] = self.doc
-        print "process, extra is ", kwargs
+        extra = {}
+        if hasattr(self, "docid"):
+            extra["documentid"] = self.docid
+        if hasattr(self, "sourcefileid"):
+            extra["sourcefileid"] = self.sourcefileid
+        extra["modulename"] = self.extra["modulename"]
+        extra["moduleversion"] = self.extra["moduleversion"]
+        kwargs["extra"] = extra
         return (msg, kwargs)
 
 def get_logger(modulename, moduleversion):
