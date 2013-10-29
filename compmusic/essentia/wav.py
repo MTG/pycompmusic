20800b4cc4a85cfb929d2b9632645696be81982a
Alastair Porter <alastair.porter@upf.edu>
2013-10-29 19:12:41 +0100

    use tempfile because lame needs to write to the file

--- compmusic/essentia/wav.py
+++ compmusic/essentia/wav.py
@@ -1,16 +1,23 @@
 import compmusic.essentia
 import subprocess
 
+import tempfile
+import os
+
 class Mp3ToWav(compmusic.essentia.EssentiaModule):
-    __version__ = "0.1"
+    __version__ = "0.3"
     __sourcetype__ = "mp3"
     __slug__ = "wav"
 
     __output__ = {"wave": {"extension": "wav", "mimetype": "audio/wave"}}
 
     def run(self, fname):
-        proclist = ["lame", "--decode", fname, "-"]
-        p = subprocess.Popen(proclist, stdout=subprocess.PIPE)
-        audiocontents = p.communicate()[0]
+        fp, tmpname = tempfile.mkstemp(".wav")
+        os.close(fp)
+        proclist = ["lame", "--decode", "-x", fname, tmpname]
+        p = subprocess.Popen(proclist)
+        p.communicate()
 
-        return {"wave": audiocontents}
+        contents = open(tmpname, "rb").read()
+        os.unlink(tmpname)
+        return {"wave": contents}
