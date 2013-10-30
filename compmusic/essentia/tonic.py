3374dbd44e9c7bce99a78e11913d3afe47766a9a
Alastair Porter <alastair.porter@upf.edu>
2013-10-30 17:51:23 +0100

    c tonic code

--- compmusic/essentia/tonic.py
+++ compmusic/essentia/tonic.py
@@ -2,6 +2,11 @@ import compmusic.essentia
 import numpy as np
 
 import essentia.standard
+import subprocess
+
+import tempfile
+import os
+from docserver import util
 
 class TonicExtract(compmusic.essentia.EssentiaModule):
     __version__ = "0.1"
@@ -14,5 +19,21 @@ class TonicExtract(compmusic.essentia.EssentiaModule):
 
         audio = essentia.standard.MonoLoader(filename=fname)()
         tonic = essentia.standard.TonicIndianArtMusic()(audio)
+
+        return {"tonic": str(tonic)}
+
+class CTonicExtract(compmusic.essentia.EssentiaModule):
+    __version__ = "0.1"
+    __sourcetype__ = "mp3"
+    __slug__ = "ctonic"
+
+    __output__ = {"tonic": {"extension": "dat", "mimetype": "text/plain"}}
+
+    def run(self, fname):
+        wavfname = util.docserver_get_filename(self.musicbrainz_id, "wav", "wave")
+        args = ["/srv/dunya/PitchCandExt_O0", "-m", "T", "-t", "V", "-i", wavfname]
+        p = subprocess.Popen(proclist, stdout=subprocess.PIPE)
+        output = p.communicate()
+        tonic = output[0]
 
         return {"tonic": str(tonic)}
