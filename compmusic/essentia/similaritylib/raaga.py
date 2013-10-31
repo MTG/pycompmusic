5a93e2a2caf61c8005c6c5ef04071936803b4efd
gopalkoduri <gopala.koduri@gmail.com>
2013-10-31 01:15:08 +0100

    code for raaga similarity is in!

---
+++ compmusic/essentia/similaritylib/raaga.py
@@ -0,0 +1,129 @@
+from __future__ import division
+from os.path import basename
+import numpy as np
+import pickle
+from glob import glob
+
+
+class Raaga:
+    def __init__(self, name, path_to_raaga_profiles):
+        self.name = name
+        self.average_hist = None
+        self.average_profile = None
+        self.path_to_raaga_profiles = path_to_raaga_profiles
+
+    @staticmethod
+    def kldiv(x, y):
+        """
+        Calculates the KL divergence D(A||B) between the
+        normalized distributions A and B.
+
+        Usage: d = kldiv(A,B)
+        """
+        eps = np.finfo(float).eps*2*len(x)
+        x = np.array(x)+eps
+        y = np.array(y)+eps
+        #eps = 0.01
+        #if max(sum(A), sum(B)) > 1+eps:
+        #   print max(sum(A), sum(B))
+        #   print "Probabilities do not sum to 1."
+        #   return np.NaN
+        if x.size != y.size:
+            print "Arguments are of different length."
+            return np.NaN
+        return (np.dot(x, np.log2(x)-np.log2(y))+np.dot(y, np.log2(y)-np.log2(x)))/2.0
+
+    def compute_average_hist(self, paths_to_pitch_files, tonics, octave_folded=True, bins=1200):
+        """
+        Computes average histogram for a list of given pitch files.
+        The pitch values are expected to be in hertz.
+        """
+        all_cents = []
+        for i in xrange(len(paths_to_pitch_files)):
+            filepath = paths_to_pitch_files[filepath]
+            data = np.loadtxt(filepath)
+
+            if data is None:
+                raise Exception("Pitch data not received.")
+
+            #Histogram
+            valid_pitch = data[:, 1]
+            tonic = tonics[i]
+            valid_pitch = [1200*np.log2(i/tonic) for i in valid_pitch if i > 0]
+            if octave_folded:
+                valid_pitch = map(lambda x: int(x % 1200), valid_pitch)
+
+            all_cents.extend(valid_pitch)
+        if not bins:
+            bins = max(all_cents)-min(all_cents)
+
+        n, bin_edges = np.histogram(all_cents, bins, normed=True)
+        bin_centers = 0.5*(bin_edges[1:]+bin_edges[:-1])
+        n = n.reshape(len(n), 1)
+        bin_centers = bin_centers.reshape(len(bin_centers, 1))
+        self.average_hist = np.append(n, bin_centers, axis=1)
+
+    def serialize_average_hist(self):
+        pickle.dump(self.average_hist, file(self.path_to_raaga_profiles + "/" + self.name + ".pickle", "w"))
+
+    def generate_image(self, x, y):
+        pass
+
+    def similar_raagas(self, n=5):
+        all_paths = glob(self.path_to_raaga_profiles + "/*.pickle")
+        source_path = self.path_to_raaga_profiles + "/" + self.name + ".pickle"
+        all_paths.remove(source_path)
+
+        distances = []
+        if self.average_hist is None:
+            self.average_hist = pickle.load(source_path)
+
+        for p in all_paths:
+            data = pickle.load(p)
+            distance = self.kldiv(self.average_hist[:, 1], data[:, 1])
+            distances.append([basename[p][:-7], distance])
+
+        distances = sorted(distances, key=lambda x: x[1])
+        distances = np.array(distances)
+        return distances[:n, 0]
+
+if __name__ == "__main__":
+    import yaml
+    import sys
+    sys.path.append("/media/CompMusic/audio/users/gkoduri/workspace/PhD/scripts")
+    import utils as u
+
+    raaga_map = yaml.load(file("/home/gopal/data/raagaClusters.yaml"))
+    pitch_files = glob("/home/gopal/data/features/pitch/*.txt")
+    mbids = [basename(mbid)[:-4] for mbid in pitch_files]
+
+    w = u.Workspace("/home/gopal/data/features/",
+                    "/home/gopal/data/features/annotations/",
+                    "/home/gopal/data/audio/Carnatic/metadata/Carnatic.yaml")
+    tonics = [w.tonic(mbid) for mbid in mbids]
+
+    raaga_mbids = {}
+    for i in xrange(len(mbids)):
+        if tonics[i] is None:
+            continue
+
+        mbid = mbids[i]
+        tag = w.get_tag("raaga", mbid)
+        raaga = w.get_raaga(tag, raaga_map)
+        if raaga == "":
+            continue
+
+        print mbid, raaga
+        if raaga in raaga_mbids.keys():
+            raaga_mbids[raaga].append([mbid, tonics[i]])
+        else:
+            raaga_mbids[raaga] = [[mbid, tonics[i]]]
+
+    for raaga in raaga_mbids.keys():
+        print r, len(raaga_mbids[r])
+        r = Raaga(raaga, "/home/gopal/data/features/raagaProfiles/")
+        pitch_files = ["/home/gopal/data/features/pitch/" + raaga_mbids[r][i][0] + ".txt"
+                       for i in raaga_mbids[r]]
+        tonics = [raaga_mbids[r][i][1] for i in raaga_mbids[r]]
+        r.compute_average_hist(pitch_files, tonics)
+        r.serialize_average_hist()
--- compmusic/essentia/similaritylib/recording.py
+++ compmusic/essentia/similaritylib/recording.py
@@ -1,17 +1,17 @@
-from numpy.linalg import norm
+import numpy as np
 
 ji_intervals = np.array([-2400, -2289, -2197, -2085, -2014, -1902, -1791,
-                          -1699, -1587, -1516, -1404, -1312, -1200, -1089,
-                          -997, -885, -814, -702, -591, -499, -387, -316,
-                          -204, -112, 0, 111, 203, 315, 386, 498, 609, 701,
-                          813, 884, 996, 1088, 1200, 1311, 1403, 1515,
-                          1586, 1698, 1809, 1901, 2013, 2084, 2196, 2288,
-                          2400, 2511, 2603, 2715, 2786, 2898, 3009, 3101,
-                          3213, 3284, 3396, 3488, 3600, 3711, 3803, 3915,
-                          3986, 4098, 4209, 4301, 4413, 4484, 4596, 4688,
-                          4800])
+                         -1699, -1587, -1516, -1404, -1312, -1200, -1089,
+                         -997, -885, -814, -702, -591, -499, -387, -316,
+                         -204, -112, 0, 111, 203, 315, 386, 498, 609, 701,
+                         813, 884, 996, 1088, 1200, 1311, 1403, 1515,
+                         1586, 1698, 1809, 1901, 2013, 2084, 2196, 2288,
+                         2400, 2511, 2603, 2715, 2786, 2898, 3009, 3101,
+                         3213, 3284, 3396, 3488, 3600, 3711, 3803, 3915,
+                         3986, 4098, 4209, 4301, 4413, 4484, 4596, 4688,
+                         4800])
 
-def fill_in(profile, _min=-2400, _max=4800]):
+def pad(profile, _min=-2400, _max=4800):
     #Fill zeros for features from intervals absent in the profile
     for interval in ji_intervals:
         if interval not in profile.keys():
@@ -31,6 +31,9 @@ def fill_in(profile, _min=-2400, _max=4800]):
     return profile
 
 def distance(profile1, profile2):
+    profile1 = pad(profile1)
+    profile2 = pad(profile2)
+
     keys = profile1.keys()
     try:
         keys.pop("skew1")
@@ -38,24 +41,24 @@ def distance(profile1, profile2):
         pass
     v1 = [profile1[k] for k in keys]
     v2 = [profile2[k] for k in keys]
-    return norm(v1-v2)
+    return np.linalg.norm(v1-v2)
 
-def kldiv(A,B):
+def kldiv(x, y):
     """
     Calculates the KL divergence D(A||B) between the 
     normalized distributions A and B.
 
     Usage: d = kldiv(A,B)
     """
-    eps = np.finfo(float).eps*2*len(A)
-    A = np.array(A)+eps
-    B = np.array(B)+eps
+    eps = np.finfo(float).eps*2*len(x)
+    x = np.array(x)+eps
+    y = np.array(y)+eps
     #eps = 0.01
     #if max(sum(A), sum(B)) > 1+eps:
     #   print max(sum(A), sum(B))
     #   print "Probabilities do not sum to 1."
     #   return np.NaN
-    if A.size != B.size:
+    if x.size != y.size:
         print "Arguments are of different length."
         return np.NaN
-    return (np.dot(A, np.log2(A)-np.log2(B))+np.dot(B, np.log2(B)-np.log2(A)))/2.0
+    return (np.dot(x, np.log2(x)-np.log2(y))+np.dot(y, np.log2(y)-np.log2(x)))/2.0
