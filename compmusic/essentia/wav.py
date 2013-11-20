b1681dd9d96694fe8fdee07148f0a0db745b0a59
Alastair Porter <alastair.porter@upf.edu>
2013-11-20 16:54:27 +0100

    get audio file length

--- compmusic/essentia/wav.py
+++ compmusic/essentia/wav.py
@@ -3,13 +3,16 @@ import subprocess
 
 import tempfile
 import os
+import wave
 
 class Mp3ToWav(compmusic.essentia.EssentiaModule):
-    __version__ = "0.4"
+    __version__ = "0.5"
     __sourcetype__ = "mp3"
     __slug__ = "wav"
 
-    __output__ = {"wave": {"extension": "wav", "mimetype": "audio/wave"}}
+    __output__ = {"wave": {"extension": "wav", "mimetype": "audio/wave"},
+                  "length": {"extension": "dat", "mimetype": "text/plain"}
+                 }
 
     def run(self, fname):
         fp, tmpname = tempfile.mkstemp(".wav")
@@ -18,6 +21,9 @@ class Mp3ToWav(compmusic.essentia.EssentiaModule):
         p = subprocess.Popen(proclist)
         p.communicate()
 
+        wfile = wave.open(tmpname, "rb")
+        length = wfile.getnframes() * 1.0 / wfile.getframerate()
+        wfile.close()
         contents = open(tmpname, "rb").read()
         os.unlink(tmpname)
-        return {"wave": contents}
+        return {"wave": contents, "length": str(int(length))}
