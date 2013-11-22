a125e18461d75e3ad34e934fb19746d4fc964a34
Alastair Porter <alastair.porter@upf.edu>
2013-11-22 15:08:49 +0100

    only return melody from pitch thing. everything else is normalised

--- compmusic/essentia/pitch.py
+++ compmusic/essentia/pitch.py
@@ -15,8 +15,7 @@ class PitchExtract(compmusic.essentia.EssentiaModule):
     __slug__ = "pitch"
 
     __depends__ = "tonic"
-    __output__ = {"pitch": {"extension": "json", "mimetype": "application/json"},
-            "histogram": {"extension": "json", "mimetype": "application/json"}}
+    __output__ = {"pitch": {"extension": "json", "mimetype": "application/json"}}
 
     def setup(self):
         # Hop size is 44100*4/900 because our smallest view is 4 seconds long
@@ -27,6 +26,41 @@ class PitchExtract(compmusic.essentia.EssentiaModule):
                           BinResolution=10,
                           GuessUnvoiced=True,
                           CentsPerBin=1)
+
+
+    def run(self, fname):
+        audioLoader = essentia.standard.EasyLoader(filename=fname)
+        monoLoader = essentia.standard.MonoLoader(filename=fname)
+        sampleRate = monoLoader.paramValue("sampleRate")
+        equalLoudness = essentia.standard.EqualLoudness(sampleRate=sampleRate)
+        audio = equalLoudness(audioLoader())
+        self.logger.info('Calculating pitch')
+        pitch = essentia.standard.PredominantMelody(hopSize=self.settings.HopSize,
+                                    frameSize=self.settings.FrameSize,
+                                    binResolution=self.settings.BinResolution,
+                                    guessUnvoiced=self.settings.GuessUnvoiced)(audio)
+
+        pitch = pitch[0]
+        self.logger.info('done')
+
+        #generating time stamps (because its equally hopped)
+        TStamps = np.array(range(0,len(pitch)))*np.float(self.settings.HopSize)/sampleRate
+        thepitch = np.array([TStamps, pitch]).transpose()
+
+        return {"pitch": thepitch.tolist()}
+
+
+class NormalisedPitchExtract(compmusic.essentia.EssentiaModule):
+    __version__ = "0.1"
+    __sourcetype__ = "mp3"
+    __slug__ = "normalisedpitch"
+
+    __depends__ = "pitch"
+
+    __output__ = {"packedpitch": {"extension": "dat", "mimetype": "application/octet-stream"},
+            "normalisedpitch": {"extension": "json", "mimetype": "application/json"},
+            "normalisedhistogram": {"extension": "json", "mimetype": "application/json"},
+            "histogram": {"extension": "json", "mimetype": "application/json"}}
 
     def get_histogram(self, pitch):
         """
@@ -45,46 +79,11 @@ class PitchExtract(compmusic.essentia.EssentiaModule):
         #The argument refers to standard deviation of gaussian kernel
         #used for smoothing.
 
-        #recording.histogram.set_smoothness(7)
+        recording.histogram.set_smoothness(100)
         #return [recording.histogram.x, recording.histogram.y]
 
         #This would instead return a raw histogram without smoothing.
-        return [recording.histogram.x.tolist(), recording.histogram.y_raw.tolist()]
-
-    def run(self, fname):
-        audioLoader = essentia.standard.EasyLoader(filename=fname)
-        monoLoader = essentia.standard.MonoLoader(filename=fname)
-        sampleRate = monoLoader.paramValue("sampleRate")
-        equalLoudness = essentia.standard.EqualLoudness(sampleRate=sampleRate)
-        audio = equalLoudness(audioLoader())
-        pitch = essentia.standard.PredominantMelody(hopSize=self.settings.HopSize,
-                                    frameSize=self.settings.FrameSize,
-                                    binResolution=self.settings.BinResolution,
-                                    guessUnvoiced=self.settings.GuessUnvoiced)(audio)
-
-        pitch = pitch[0]
-
-        #generating time stamps (because its equally hopped)
-        TStamps = np.array(range(0,len(pitch)))*np.float(self.settings.HopSize)/sampleRate
-        dump = np.array([TStamps, pitch]).transpose()
-
-        p_histogram = self.get_histogram(dump)
-        thepitch = dump.tolist()
-
-        return {"pitch": thepitch,
-                "histogram": p_histogram}
-
-
-class NormalisedPitchExtract(compmusic.essentia.EssentiaModule):
-    __version__ = "0.1"
-    __sourcetype__ = "mp3"
-    __slug__ = "normalisedpitch"
-
-    __depends__ = "pitch"
-
-    __output__ = {"packedpitch": {"extension": "dat", "mimetype": "application/octet-stream"},
-            "normalisedpitch": {"extension": "json", "mimetype": "application/json"},
-            "normalisedhistogram": {"extension": "json", "mimetype": "application/json"}}
+        return zip(recording.histogram.x.tolist(), recording.histogram.y.tolist())
 
     def normalise_pitch(self, pitch):
         eps = np.finfo(np.float).eps
@@ -102,8 +101,13 @@ class NormalisedPitchExtract(compmusic.essentia.EssentiaModule):
         return normalised_pitch
 
     def run(self, fname):
+        eps = np.finfo(np.float).eps
         pitch = util.docserver_get_json(self.musicbrainz_id, "pitch", "pitch")
         histogram = util.docserver_get_json(self.musicbrainz_id, "pitch", "histogram")
+
+        histpitch = 1200 * np.log2(pitch + eps)
+        hist_in = np.array([TStamps, histpitch]).transpose()
+        p_histogram = self.get_histogram(hist_in)
 
         normalised_pitch = self.normalise_pitch(pitch)
         normalised_pitch = {"normalised": normalised_pitch}
