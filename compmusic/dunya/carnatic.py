cbc50ecddc213db0e17115f1b20277f54b5377e7
Alastair Porter <alastair.porter@upf.edu>
2014-01-29 18:26:24 +0100

    all list and detail carnatic api methods + docs (mtg/dunya#41)

--- compmusic/dunya/carnatic.py
+++ compmusic/dunya/carnatic.py
@@ -4,27 +4,149 @@ logger = logging.getLogger("dunya")
 
 import conn
 
-def get_recordings(artistid=None, concertid=None, raaga=None, taala=None, instrument=None, work=None):
+def get_recordings():
+    """ Get a list of carnatic recordings in the database.
+    This function will automatically page through API results.
+    Returns: A list of dictionaries containing recording information.
+    {"mbid": Musicbrainz recording id,
+     "title": Title of the recording
+    }
+    For additional information about each recording use the
+    `get_recording` method. """
     return conn._get_paged_json("api/carnatic/recording")
 
-def get_artists(raaga=None, taala=None, instrument=None):
+def get_recording(rmbid):
+    """ Get specific information about a recording. 
+    Arguments:
+      rmbid: A recording mbid
+    Returns: mbid, title, artists, raaga, taala, work
+             artists includes performance relationships attached
+             to the recording, the release, and the release artists.
+    """
+    return conn._dunya_query_json("api/carnatic/recording/%s" % rmbid)
+
+def get_artists():
+    """ Get a list of Carnatic artists in the database.
+    This function will automatically page through API results.
+    Returns: A list of dictionaries containing artist information.
+    {"mbid": Musicbrainz artist id,
+     "name": Name of the artist
+    }
+    For additional information about each artist use the
+    `get_artist` method. """
     return conn._get_paged_json("api/carnatic/artist")
 
+def get_artist(ambid):
+    """ Get specific information about an artist.
+    Arguments:
+      ambid: An artist mbid
+    Returns: mbid, name, concerts, instruments, recordings
+             concerts, instruments and recordings include information
+             from recording- and release-level relationships, as
+             well as release artists
+    """
+    return conn._dunya_query_json("api/carnatic/artist/%s" % ambid)
+
 def get_concerts():
+    """ Get a list of Carnatic concerts in the database.
+    This function will automatically page through API results.
+    Returns: A list of dictionaries containing concert information.
+    {"mbid": Musicbrainz concert id,
+     "title": title of the concert
+    }
+    For additional information about each concert use the
+    `get_concert` method. """
     return conn._get_paged_json("api/carnatic/concert")
 
-def get_concert(mbid):
-    return conn._dunya_query_json("api/carnatic/concert/%s" % mbid)
+def get_concert(cmbid):
+    """ Get specific information about a concert. 
+    Arguments:
+      cmbid: A concert mbid
+    Returns: mbid, title, artists, tracks
+             artists includes performance relationships attached
+             to the recordings, the release, and the release artists.
+    """
+    return conn._dunya_query_json("api/carnatic/concert/%s" % cmbid)
 
 def get_works():
+    """ Get a list of Carnatic works in the database.
+    This function will automatically page through API results.
+    Returns: A list of dictionaries containing work information.
+    {"mbid": Musicbrainz work id,
+     "name": work name
+    }
+    For additional information about each work use the
+    `get_work` method. """
     return conn._get_paged_json("api/carnatic/work")
 
+def get_work(wmbid):
+    """ Get specific information about a work.
+    Arguments:
+      wmbid: A work mbid
+    Returns: mbid, title, composer, raagas, taalas, recordings
+    """
+    return conn._dunya_query_json("api/carnatic/work/%s" % wmbid)
+
 def get_raagas():
+    """ Get a list of Carnatic raagas in the database.
+    This function will automatically page through API results.
+    Returns: A list of dictionaries containing raaga information.
+    {"id": raaga id,
+     "name": name of the raaga
+    }
+    For additional information about each raaga use the
+    `get_raaga` method. """
     return conn._get_paged_json("api/carnatic/raaga")
 
+def get_raaga(rid):
+    """ Get specific information about a raaga.
+    Arguments:
+      rid: A raaga id
+    Returns: id, name, artists, works, composers
+             artists includes artists with recording- and release-
+             level relationships to a recording with this raaga
+    """
+    return conn._dunya_query_json("api/carnatic/raaga/%s" % str(rid))
+
 def get_taalas():
+    """ Get a list of Carnatic taalas in the database.
+    This function will automatically page through API results.
+    Returns: A list of dictionaries containing taala information.
+    {"id": taala id,
+     "name": name of the taala
+    }
+    For additional information about each taala use the
+    `get_taala` method. """
     return conn._get_paged_json("api/carnatic/taala")
 
+def get_taala(tid):
+    """ Get specific information about a taala.
+    Arguments:
+      tid: A taala id
+    Returns: id, name, artists, works, composers
+             artists includes artists with recording- and release-
+             level relationships to a recording with this raaga
+    """
+    return conn._dunya_query_json("api/carnatic/taala/%s" % str(tid))
+
 def get_instruments():
+    """ Get a list of Carnatic instruments in the database.
+    This function will automatically page through API results.
+    Returns: A list of dictionaries containing instrument information.
+    {"id": instrument id,
+     "name": Name of the instrument
+    }
+    For additional information about each instrument use the
+    `get_instrument` method. """
     return conn._get_paged_json("api/carnatic/instrument")
+
+def get_instrument(iid):
+    """ Get specific information about an instrument.
+    Arguments:
+      iid: An instrument id
+    Returns: id, name, artists
+             artists includes artists with recording- and release-
+             level performance relationships of this instrument.
+    """
+    return conn._dunya_query_json("api/carnatic/instrument/%s" % str(iid))
 
--- compmusic/dunya/conn.py
+++ compmusic/dunya/conn.py
@@ -6,10 +6,18 @@ import logging
 logger = logging.getLogger("dunya")
 
 HOSTNAME = "dunya.compmusic.upf.edu"
+TOKEN = None
+
+class ConnectionError(Exception):
+    pass
 
 def set_hostname(hostname):
     global HOSTNAME
     HOSTNAME = hostname
+
+def set_token(token):
+    global TOKEN
+    TOKEN = token
 
 def _get_paged_json(path, **kwargs):
     ret = []
@@ -18,15 +26,19 @@ def _get_paged_json(path, **kwargs):
     nxt = res.get("next")
     while nxt:
         res = _json_url_query(nxt)
+        res = res.json()
         nxt = res.get("next")
         ret.extend(res.get("results", []))
     return ret
 
-def _json_url_query(url):
+def _dunya_url_query(url):
     logger.debug("query to '%s'"%url)
-    g = requests.get(url)
+    if not TOKEN:
+        raise ConnectionError("You need to authenticate with `set_token`")
+    headers = {"Authorization": "Token %s" % TOKEN}
+    g = requests.get(url, headers=headers)
     g.raise_for_status()
-    return g.json()
+    return g
 
 def _dunya_query(path, **kwargs):
     if not kwargs:
@@ -42,10 +54,7 @@ def _dunya_query(path, **kwargs):
         urllib.urlencode(kwargs),
         ''
     ))
-    logger.debug("query to '%s'"%url)
-    g = requests.get(url)
-    g.raise_for_status()
-    return g
+    return _dunya_url_query(url)
 
 def _dunya_query_json(path, **kwargs):
     """Make a query to dunya and expect the results to be JSON"""
