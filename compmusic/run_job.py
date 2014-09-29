6b9a82e0c2ff43be6d15b4fb83eeef44ef555e61
Alastair Porter <alastair.porter@upf.edu>
2014-09-29 14:50:49 +0200

    Update reports on job runner script
    Report modules that can't be imported
    Report duplicate slug names
    Fixes mtg/dunya#165

--- compmusic/run_job.py
+++ compmusic/run_job.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 
-import compmusic
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import imp
 import importlib
 import inspect
 import argparse
-import os
-import sys
 import logging
-
+import json
+import compmusic
 from compmusic import extractors
 
 logger = logging.getLogger("extractor")
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-logger.addHandler(ch)
+#ch = logging.StreamHandler()
+#ch.setLevel(logging.DEBUG)
+#logger.addHandler(ch)
 
 def _get_module_by_path(modulepath):
     mod, clsname = modulepath.rsplit(".", 1)
-    package = importlib.import_module(mod)
-    cls = getattr(package, clsname)
-    return cls
+    try:
+        package = importlib.import_module(mod)
+        cls = getattr(package, clsname)
+        return cls
+    except ImportError:
+        logger.warn("Cannot import the module: %s" % mod)
+        logger.warn("Try it in a terminal and see what the error is")
 
 def _get_module_by_slug(slug):
     # Get all files in the module
@@ -28,16 +34,36 @@ def _get_module_by_slug(slug):
     modules = set(["compmusic.extractors.%s" % os.path.splitext(module)[0]
                 for module in os.listdir(dirname) if module.endswith(".py")])
 
+    unloaded = []
+    matching = []
     for m in modules:
         try:
             loaded = importlib.import_module(m)
             for name, mod in inspect.getmembers(loaded, inspect.isclass):
                 if issubclass(mod, extractors.ExtractorModule) and name != "ExtractorModule":
                     if mod.__slug__ == slug:
-                        return mod
+                        matching.append(mod)
         except ImportError:
-            pass
-    return None
+            unloaded.append(m)
+
+    if unloaded:
+        logger.warn("Failed to load these modules due to an import error, check that you have all their dependencies installed")
+        for u in unloaded:
+            logger.warn(u)
+
+    if len(matching) > 1:
+        logger.warn("Found more than one module with the same slug. Slugs must be unique")
+        logger.warn("For slug: %s" % slug)
+        for m in matching:
+            logger.warn("  %s" % m)
+    elif len(matching) == 1:
+        return matching[0]
+    else:
+        logger.warn("Cannot find a module with the slug: %s" % slug)
+        logger.warn("Check that you have spelt it correctly")
+        if unloaded:
+            logger.warn("or that the module it is in can be loaded (is it in one of the above failed modules?)")
+        return None
 
 def load_module(modulepath):
     if "." in modulepath:
@@ -61,19 +87,23 @@ def save_data(module, data):
         for i in range(len(d)):
             fname = "%s-%s-%s.%s" % (mbid, key, i, ext)
             print "Writing output for type %s to %s" % (key, fname)
-            open(fname, "wb").write(d[i])
+            if output[key]["mimetype"] == "application/json":
+                output = json.dumps(d[i])
+            else:
+                output = d[i]
+            open(fname, "wb").write(output)
 
 def run_file(module, filename, mbid=None):
     if not mbid:
-        md = compmusic.file_metadata(filename)
-        mbid = md["meta"]["recordingid"]
-
+        if filename.lower().endswith(".mp3"):
+            md = compmusic.file_metadata(filename)
+            mbid = md["meta"]["recordingid"]
     if mbid:
         module.musicbrainz_id = mbid
         ret = module.run(filename)
         save_data(module, ret)
     else:
-        print >>sys.stderr, "Cannot find a mbid in this file. Use the mbid argument"
+        logging.error("Cannot find a mbid in this file. Use the mbid argument")
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -89,7 +119,4 @@ if __name__ == "__main__":
     themod = load_module(module)
     if themod:
         run_file(themod, filename, mbid)
-    else:
-        print >>sys.stderr, "could not load the module. does it exist and can"
-        print >>sys.stderr, "you load it in a python shell?"
 
