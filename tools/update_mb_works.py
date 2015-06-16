20101dba441b25531d5303155fc8f6997dcb507b
Andres Ferraro <aferraro@dcodesoft.com>
2015-06-16 16:54:26 -0300

    Changed script to generate html links separated in 2 lists

--- tools/update_mb_works.py
+++ tools/update_mb_works.py
@@ -38,6 +38,10 @@ symbtrmu2_url = 'http://dunya.compmusic.upf.edu/document/by-id/%s/symbtrmu2'
 dunya_fuzzy_url = 'http://dunya.compmusic.upf.edu/api/makam/fuzzy'
 
 mb_cache = {}
+count_missing_mu2 = 0
+count_matched_mu2 = 0
+count_missing_dunya = 0
+
 
 def get_symbtrmu2(work_mbid):
     # Get symbtrmu2 file and extract makam, form and usul
@@ -50,7 +54,8 @@ def get_symbtrmu2(work_mbid):
     data = {"50": None, "51": None, "57": None}
     try:
         response = urllib2.urlopen(file_url)
-        for line in response.readlines():
+        document = response.read()
+        for line in document.splitlines():
             info = line.decode('iso8859-9').split("\t")
             if info[0] in data.keys():
                 data[info[0]] = info[7]
@@ -59,8 +64,11 @@ def get_symbtrmu2(work_mbid):
     return {"makam": data["50"], "usul": data["51"], "form": data["57"]}
 
 def get_mb_recording(collection_mbid, output_file):
+    global symbtrmu2_url, count_matched_mu2, count_missing_mu2, count_missing_dunya
     # Get the collection from musicbrainz and extract the recordings
     print "Retrieving information from mb collection: %s" % collection_mbid
+    res = {"mu2": [], "mb": []}
+
     rec_list = []
     collection = mb.get_releases_in_collection(collection_mbid)
     for rel in collection["collection"]["release-list"]:
@@ -77,7 +85,7 @@ def get_mb_recording(collection_mbid, output_file):
         rec_tags = []
         if "tag-list" in work_rels["recording"]:
             rec_tags = work_rels["recording"]["tag-list"]
-        
+
         works = []
         if "work-relation-list" in work_rels["recording"]:
             works = work_rels["recording"]["work-relation-list"]
@@ -90,6 +98,7 @@ def get_mb_recording(collection_mbid, output_file):
                 work_tags = work["work"]["tag-list"] 
             
             mu2 = get_symbtrmu2(work["work"]["id"])
+            title = work["work"]["title"].encode("utf-8")
 
             # get alias for each tag 
             try:
@@ -98,11 +107,12 @@ def get_mb_recording(collection_mbid, output_file):
                 mu2_makam = get_fuzzy("makam", mu2["makam"])
                 mu2_form = get_fuzzy("form", mu2["form"])
                 mu2_usul = get_fuzzy("usul", mu2["usul"])
-                
+
                 # compare results of 3 sources
                 makam, usul, form = (None, None, None)
                 if mu2_makam and mu2_usul and mu2_form:
                     #There is mu2 file with makam usul and form
+                    count_matched_mu2 += 1
 
                     if mu2_makam == rec_makam and mu2_usul == rec_usul and mu2_form == rec_form:
                         if work_makam == rec_makam and work_usul == rec_usul and work_form == rec_form:
@@ -118,21 +128,41 @@ def get_mb_recording(collection_mbid, output_file):
                         print "There's a difference between mu2 file and mb recording information."
                         print "Recording form: %s usul: %s makam: %s" % (rec_form, rec_usul, rec_makam)
                         print "Mu2 form: %s usul: %s makam: %s" % (mu2_form, mu2_usul, mu2_makam)
+
+                    if makam and usul and form:
+                        try:
+                            new_link = update_mb_work(work["work"]["id"], title, makam, form, usul, symbtrmu2_url % work["work"]["id"])
+                            if new_link:
+                                res['mu2'].append(new_link)
+                        except ElementNotFoundException, e:
+                            print "Couldn't generate link because element not present in MB"
+
                 elif (not work_makam and not work_usul and not work_form) or \
                         (work_makam == rec_makam and work_usul == rec_usul and work_form == rec_form):
                     # If theres no mu2 file we only consider mb information
-                    makam = rec_makam
-                    usul = rec_usul
-                    form = rec_form
-                
-                if makam and usul and form:
-                    try:
-                        update_mb_work(work["work"]["id"], makam, form, usul, output_file)
-                    except ElementNotFoundException, e:
-                        print "Couldn't generate link because element not present in MB"
+                    count_missing_mu2 += 1
+                    if rec_makam and rec_usul and rec_form:
+                        try:
+                            new_link = update_mb_work(work["work"]["id"], title, rec_makam, rec_form, rec_usul, None)
+                            res['mb'].append(new_link)
+                        except ElementNotFoundException, e:
+                            print "Couldn't generate link because element not present in MB"
+
             except AliasNotFoundException, e:
-                 print "Skipping work because alias not found on Dunya"
-                 
+                count_missing_dunya += 1
+                print "Skipping work because alias not found on Dunya"
+
+    with open(output_file, "a+") as append_file:
+        append_file.write("<h1> List of links generated from MU2 files information </h1>")
+        for i in res['mu2']:
+            append_file.write(i)
+        append_file.write("<h1> List of links generated from recording tags </h1>")
+        for i in res['mb']:
+            append_file.write(i)
+
+    print "Completed with missing mu2 files: %d, matched mu2 files: %d, missing on dunya: %d" % \
+            (count_missing_mu2, count_matched_mu2, count_missing_dunya) 
+
 def get_tags(tags):
     makam, usul, form = (None, None, None)
     for t in tags:
@@ -152,19 +182,18 @@ def get_tags(tags):
     return get_fuzzy("makam", makam), get_fuzzy("usul", usul), get_fuzzy("form", form)
 
 def get_fuzzy(key, value):
-    global dunya_fuzzy_url
+    global dunya_fuzzy_url, auth_token
     if not value:
         return None
-    return query_dunya_api(dunya_fuzzy_url, {key: value.encode("utf-8")})
+    return query_dunya_api(dunya_fuzzy_url, {key: value.encode("utf-8")}, auth_token, 'name')
 
-def query_dunya_api(url, params):
-    global auth_token
+def query_dunya_api(url, params, auth_token, retrieve_attr):
     try:
         opener = urllib2.build_opener()
         opener.addheaders = [("Authorization", "Token " + auth_token)]
         response = opener.open(url + "?"+urllib.urlencode(params))
         res = json.loads(response.read())
-        return res['name'].encode('utf-8')
+        return res[retrieve_attr].encode('utf-8')
     except Exception, e:
         print 'ERROR retrieving dunya information:'  
         print url + "?"+urllib.urlencode(params)
@@ -188,8 +217,8 @@ def get_session_id():
             return cookie.value
     return ""
 
-def update_mb_work(work_mbid, makam, form, usul, output_file):
-    global domain, work_url, mb_cache
+def update_mb_work(work_mbid, work_name, makam, form, usul, mu2_file):
+    global domain, work_url, mb_cache, auth_token
     
     if len(mb_cache) == 0:
         print "Updating MB work %s, generating session id" % work_mbid
@@ -223,7 +252,7 @@ def update_mb_work(work_mbid, makam, form, usul, output_file):
         raise ElementNotFoundException("Form %s not present in Musicbrainz" % form)
     elif not usul_id:
         raise ElementNotFoundException("Usul %s not present in Musicbrainz" % usul)
-    
+
     # 15 -> Makam
     # 16 -> Form
     # 17 -> Usul
@@ -234,15 +263,25 @@ def update_mb_work(work_mbid, makam, form, usul, output_file):
         "edit-work.attributes.1.value": form_id,
         "edit-work.attributes.2.type_id": 17,
         "edit-work.attributes.2.value": usul_id,
-        } 
+        }
+
+    if mu2_file:
+        symbtr_name = query_dunya_api(mu2_file.replace("/symbtrmu2", ""), {}, auth_token, 'title')
+        data['edit-work.edit_note'] = "Makam, Form and Usul added from symbtr %s" % symbtr_name
     
-    # Make update
+    # Generate update link
     
+    link_html = '<div><a href="%s">%s</a>'
+    mu2_html = '<a href="%s">Mu2 file</a>'
+    end_html = '</div>\n'
     gen_url = domain + (work_url % work_mbid) + "?" + urllib.urlencode(data) 
     
-    print "New mb work edition link generated:"
-    with open(output_file, "a+") as append_file:
-        append_file.write(gen_url + "\n")
+    print "New mb work edition link generated"
+    ret = link_html % (gen_url, work_name)
+    if mu2_file:
+        ret = ret + mu2_html % ( mu2_file)
+    ret = ret + end_html
+    return ret
 
 class AliasNotFoundException(Exception):
     pass
