eaf3813c7735782f59fde2c58cc7734f9f9d4344
georgid <georgi.dzhambazov@upf.edu>
2016-02-02 19:45:29 +0100

    -refactoring LyricsAlign class: has only run method, all other logic moved to https://github.com/georgid/AlignmentDuration/blob/IntegratedPycompMusic/align/LyricsAligner.py
    - without need to use docserver.util from dunya

--- compmusic/extractors/makam/lyricsalign.py
+++ compmusic/extractors/makam/lyricsalign.py
@@ -1,33 +1,24 @@
 import os
 import sys
-parentDir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__) ), os.path.pardir)) 
-# pathPycompmusic = os.path.join(parentDir, 'pycompmusic')
-# if pathPycompmusic not in sys.path:
-#     sys.path.append(pathPycompmusic)
+from compmusic.extractors.wav import Mp3ToWav
+import subprocess
+parentDir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__) ), os.path.pardir,  os.path.pardir,  os.path.pardir,  os.path.pardir)) 
+pathAlignmentDur = os.path.join(parentDir, 'AlignmentDuration')
+if pathAlignmentDur not in sys.path:
+    sys.path.append(pathAlignmentDur)
 
 
 import compmusic.extractors
-from docserver import util
+# from docserver import util
 from compmusic import dunya
 from compmusic.dunya import makam
 import tempfile
+import essentia.standard
 
-currDir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) )
-modelDIR = currDir + '/model/'
-HMM_LIST_URI = modelDIR + '/monophones0'
-MODEL_URI = modelDIR + '/hmmdefs9gmm9iter'
+from align.LyricsAligner  import constructSymbTrTxtURI, alignRecording
 
-from align.MakamScore import loadMakamScore, loadMakamScore2
-from hmm.examples.main import loadSmallAudioFragment
-from align.Decoder import Decoder
-from align.SectionLink import SectionLink
 dunya.set_token("69ed3d824c4c41f59f0bc853f696a7dd80707779")
 
-
-from utilsLyrics.Utilz import writeListOfListToTextFile, writeListToTextFile,\
-    getMeanAndStDevError, getSectionNumberFromName, readListOfListTextFile, readListTextFile, getMelodicStructFromName, tokenList2TabFile
-
-from htkparser.htk_converter import HtkConverter
 
 
 class LyricsAlign(compmusic.extractors.ExtractorModule):
@@ -38,6 +29,9 @@ class LyricsAlign(compmusic.extractors.ExtractorModule):
             "alignedLyricsSyllables": {"extension": "json", "mimetype": "application/json"},
             }
     
+
+
+
     def run(self, musicbrainzid, fname):
         
         rec_data = dunya.makam.get_recording(musicbrainzid )
@@ -49,143 +43,63 @@ class LyricsAlign(compmusic.extractors.ExtractorModule):
                 raise Exception('More than one work for recording %s Not implemented!' % musicbrainzid)
         
         w = rec_data['works'][0]
-        symbtrtxtURI = util.docserver_get_symbtrtxt(w['mbid'])
+        #symbtrtxtURI = util.docserver_get_symbtrtxt(w['mbid'])
+        
+        URI_dataset = '/Users/joro/Downloads/turkish-makam-lyrics-2-audio-test-data-synthesis/'
+        symbtrtxtURI, symbTrCompositionName  = constructSymbTrTxtURI(URI_dataset, w['mbid'])
+        
         if not symbtrtxtURI:
                 return
         
         # TODO: if symbTr does not have second verse, continue
-        sectionLinksURI = dunya.docserver.get_document_as_json(musicbrainzid, "scorealign", "sectionlinks", 1, version="0.2")
+        sectionLinks = dunya.docserver.get_document_as_json(musicbrainzid, "scorealign", "sectionlinks", 1, version="0.2")
 
         sectionMetadataURI = dunya.docserver.get_document_as_json(w['mbid'], "metadata", "metadata", 1, version="0.1")
         
-        sectionMetadataURI = dunya.docserver.get_document_as_json(w['mbid'], "metadata", "metadata", 1, version="0.1")
-        audioFileURI, created = util.docserver_get_wav_filename(musicbrainzid)
-
+        extractedPitch = dunya.docserver.get_document_as_json(musicbrainzid, "initialmakampitch", "pitch", 1, version="0.6")
+         
         outputDir = tempfile.mkdtemp()
         
-        totalDetectedTokenList = alignRecording(symbtrtxtURI, sectionMetadataURI, sectionLinksURI, audioFileURI, outputDir)
+#  on dunya server       
+#         wavFileURI, created = util.docserver_get_wav_filename(musicbrainzid)
+
+# on other computer         
+        wavFileURI = self.download_wav(musicbrainzid, outputDir)
+               
+        
+        totalDetectedTokenList = alignRecording(symbtrtxtURI, sectionMetadataURI, sectionLinks, wavFileURI, extractedPitch, outputDir)
 
         ret = {'alignedLyricsSyllables':{} }
         ret['alignedLyricsSyllables'] = totalDetectedTokenList
         return ret
 
 
+    def download_wav(self, musicbrainzid, outputDir):
+        mp3FileURI = dunya.makam.download_mp3(musicbrainzid, outputDir)
+    ###### mp3 to Wav: way 1
+    #         newName = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'test.mp3')
+    #         os.rename(mp3FileURI, newName )
+    #         mp3ToWav = Mp3ToWav()
+    #         wavFileURI = mp3ToWav.run('dummyMBID', newName)
+        
+        ###### mp3 to Wav: way 2
+        wavFileURI = os.path.splitext(mp3FileURI)[0] + '.wav'
+        pipe = subprocess.Popen(['/usr/local/bin/ffmpeg', '-i', mp3FileURI, wavFileURI])
+        pipe.wait()
+    
+    ### stereo to mono
+        sampleRate = 44100
+        loader = essentia.standard.MonoLoader(filename=wavFileURI, sampleRate=sampleRate)
+        audio = loader()
+        monoWriter = essentia.standard.MonoWriter(filename=wavFileURI)
+        monoWriter(audio)
+        return wavFileURI
+
 # TODO: delete output        
 #         if os.path.isfile(os.path.join(output, 'sectionLinks.json')):
 #                 os.remove(os.path.join(output, 'sectionLinks.json'))
         
+if __name__=='__main__':
+        la = LyricsAlign()
+        la.run('b49c633c-5059-4658-a6e0-9f84a1ffb08b', 'testName') 
         
-        
-def alignRecording(symbtrtxtURI, sectionMetadataURI, sectionLinksURI, audioFileURI, outputDir):
-         
-        htkParser = HtkConverter()
-        htkParser.load(MODEL_URI, HMM_LIST_URI)
-        
-        recordingNoExtURI = os.path.splitext(audioFileURI)[0]
-        
-        sectionLinks = _loadsectionTimeStampsLinksNew( sectionLinksURI) 
-        makamScore = loadMakamScore2(symbtrtxtURI, sectionMetadataURI )
-        
-        tokenLevelAlignedSuffix = '.alignedLyrics' 
-        totalDetectedTokenList = []
-        for  currSectionLink in sectionLinks :
-            
-    
-            lyrics = makamScore.getLyricsForSection(currSectionLink.melodicStructure)
-    
-            lyricsStr = lyrics.__str__()
-        
-            if not lyricsStr or lyricsStr=='None' or  lyricsStr =='_SAZ_':
-                print("skipping sectionLink {} with no lyrics ...".format(currSectionLink.melodicStructure))
-                continue
-            
-            withSynthesis = True
-            withOracle = False
-            oracleLyrics = ''
-            usePersistentFiles = True
-            detectedTokenList, detectedPath, maxPhiScore = alignSectionLink( lyrics, withSynthesis, withOracle, oracleLyrics, [],  usePersistentFiles, tokenLevelAlignedSuffix, recordingNoExtURI, currSectionLink, htkParser)
-            
-            totalDetectedTokenList.extend(detectedTokenList)
-        
-        return totalDetectedTokenList
-            
-def  alignSectionLink( lyrics, withSynthesis, withOracle, lyricsWithModelsORacle, listNonVocalFragments,   usePersistentFiles, tokenLevelAlignedSuffix,  URIrecordingNoExt, currSectionLink, htkParser):
-        '''
-        wrapper top-most logic method
-        '''
-        if withOracle:
-    
-            # synthesis not needed really in this setting. workaround because without synth takes whole recording  
-            withSynthesis = 1
-            
-    #     read from file result
-        URIRecordingChunkResynthesizedNoExt =  URIrecordingNoExt + "_" + str(currSectionLink.beginTs) + '_' + str(currSectionLink.endTs)
-        detectedAlignedfileName = URIRecordingChunkResynthesizedNoExt + tokenLevelAlignedSuffix
-        if not os.path.isfile(detectedAlignedfileName):
-            #     ###### extract audio features
-            lyricsWithModels, obsFeatures, URIrecordingChunk = loadSmallAudioFragment(lyrics,  URIrecordingNoExt, URIRecordingChunkResynthesizedNoExt, bool(withSynthesis), currSectionLink, htkParser)
-            
-        # DEBUG: score-derived phoneme  durations
-    #     lyricsWithModels.printPhonemeNetwork()
-    #     lyricsWithModels.printWordsAndStates()
-            alpha = 0.97
-            decoder = Decoder(lyricsWithModels, URIRecordingChunkResynthesizedNoExt, alpha)
-        #  TODO: DEBUG: do not load models
-        # decoder = Decoder(lyrics, withModels=False, numStates=86)
-        #################### decode
-            
-            if withOracle:
-                detectedTokenList = decoder.decodeWithOracle(lyricsWithModelsORacle, URIRecordingChunkResynthesizedNoExt )
-            else:
-                detectedTokenList = decoder.decodeAudio(obsFeatures, listNonVocalFragments, usePersistentFiles)
-            
-            phiOptPath = decoder.path.phiOptPath
-            detectedPath = decoder.path.pathRaw
-            tokenList2TabFile(detectedTokenList, URIRecordingChunkResynthesizedNoExt, tokenLevelAlignedSuffix, currSectionLink.beginTs)
-         
-           
-            
-        ### VISUALIZE result 
-    #         decoder.lyricsWithModels.printWordsAndStatesAndDurations(decoder.path)
-        
-        else:   
-                print "{} already exists. No decoding".format(detectedAlignedfileName)
-                detectedTokenList = readListOfListTextFile(detectedAlignedfileName)
-                if withOracle:
-                    outputURI = URIRecordingChunkResynthesizedNoExt + '.path_oracle'
-                else:
-                    outputURI = URIRecordingChunkResynthesizedNoExt + '.path'
-                
-                detectedPath = readListTextFile(outputURI)
-                
-                # TODO: store persistently
-                phiOptPath = 0
-       
-    
-        return detectedTokenList, detectedPath, phiOptPath
-
-
-def _loadsectionTimeStampsLinksNew(URILinkedSectionsFile):
-        import json
-        with open(URILinkedSectionsFile) as b:
-            sectionLinks = json.load(b)
-    
-        sectionsLinks = []               
-        sectionLinks = sectionLinks['links']
-        for sectionAnno in sectionLinks:
-                        
-                        melodicStruct = sectionAnno['name']
-                        
-                        beginTimeStr = str(sectionAnno['time'][0])
-                        beginTimeStr = beginTimeStr.replace("[","")
-                        beginTimeStr = beginTimeStr.replace("]","")
-                        beginTs =  float(beginTimeStr)
-                            
-                        endTimeStr = str(sectionAnno['time'][1])
-                        endTimeStr = endTimeStr.replace("[","")
-                        endTimeStr = endTimeStr.replace("]","")
-                        endTs =  float(endTimeStr)
-                        currSectionLink = SectionLink (melodicStruct, beginTs, endTs) 
-                        sectionsLinks.append(currSectionLink )
-                    
-        return sectionsLinks
