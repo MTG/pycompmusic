9bcb0c9dbe40dd898d5ac964e446edff27b4889a
Andres Ferraro <andres.ferraro@upf.edu>
2016-03-01 16:48:20 +0100

    Adding new extractor to generate svg from musicxml

---
+++ compmusic/extractors/musicxml2svg.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013,2014 Music Technology Group - Universitat Pompeu Fabra
+#
+# This file is part of Dunya
+#
+# Dunya is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation (FSF), either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see http://www.gnu.org/licenses/
+
+import os
+import re
+import json
+import tempfile
+
+import compmusic.extractors
+from subprocess import call
+from os.path import isfile, join
+from makam.musicxml2lilypond import ScoreConverter
+
+class Musicxml2Svg(compmusic.extractors.ExtractorModule):
+    _version = "0.1"
+    _sourcetype = "symbtrxml"
+    _slug = "svgscore"
+    _output = {
+            "score": {"extension": "svg", "mimetype": "image/svg+xml", "parts": True},
+    }
+
+
+    def run(self, musicbrainzid, fpath):
+        conv = ScoreConverter(fpath)
+        conv.run()
+
+        tmp_dir = tempfile.mkdtemp()
+        call(["lilypond", '-dpaper-size=\"junior-legal\"', "-dbackend=svg", "-o" "%s" % (tmp_dir), fpath.replace(".xml",".ly")])
+
+        ret = {'xmlscore': ''}
+        musicxml = open(tmpxml)
+        ret['xmlscore'] = musicxml.read()
+        musicxml.close()
+
+        os.unlink(tmpxml)
+
+        regex = re.compile(r'.*<a style="(.*)" xlink:href="textedit:\/\/\/.*:([0-9]+):([0-9]+):([0-9]+)">.*')
+        files = [os.path.join(tmp_dir, f) for f in os.listdir(tmp_dir)]
+        files = filter(os.path.isfile, files)
+        files.sort(key=lambda x: os.path.getmtime(x))
+
+        for f in files:
+            if f.endswith('.svg'):
+                svg_file = open(f)
+                score = svg_file.read()
+                ret['score'].append(regex.sub(r'<a style="\1" id="l\2-f\3-t\4" from="\3" to="\4">',score))
+                svg_file.close()
+                os.remove(f)
+        os.rmdir(tmp_dir)
+        return ret
+
+
+
