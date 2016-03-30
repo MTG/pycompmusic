6e3c0a84b27d19821d0c5a0b3708097063bb591c
georgid <georgi.dzhambazov@upf.edu>
2016-03-30 17:55:09 +0200

    flag  WITH_SECTION_ANNOTATIONS added in lyrics align: it fetches section annotations and uses it instead of output of section linking.

--- compmusic/extractors/makam/lyricsalign.py
+++ compmusic/extractors/makam/lyricsalign.py
@@ -5,7 +5,9 @@ Created on Feb 3, 2016
 '''
 
 import sys
-from compmusic.extractors.wav import Mp3ToWav
+import os
+import urllib2
+import json
 # parentDir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__) ), os.path.pardir,  os.path.pardir,  os.path.pardir,  os.path.pardir)) 
 # pathAlignmentDur = os.path.join(parentDir, 'AlignmentDuration')
 # if pathAlignmentDur not in sys.path:
@@ -13,7 +15,7 @@ from compmusic.extractors.wav import Mp3ToWav
 
 
 import compmusic.extractors
-from docserver import util
+# from docserver import util
 from compmusic import dunya
 from compmusic.dunya import makam
 import tempfile
@@ -24,6 +26,7 @@ from align.LyricsAligner  import downloadSymbTr,  alignRecording, download_wav, 
 
 dunya.set_token("69ed3d824c4c41f59f0bc853f696a7dd80707779")
 
+WITH_SECTION_ANNOTATIONS = 1
 
 
 class LyricsAlign(compmusic.extractors.ExtractorModule):
@@ -35,6 +38,9 @@ class LyricsAlign(compmusic.extractors.ExtractorModule):
             "sectionlinks": {"extension": "json", "mimetype": "application/json"},
             }
     
+
+
+
 
 
 
@@ -51,10 +57,10 @@ class LyricsAlign(compmusic.extractors.ExtractorModule):
         w = rec_data['works'][0]
         outputDir = tempfile.mkdtemp()
 # on dunya server
-        symbtrtxtURI = util.docserver_get_symbtrtxt(w['mbid'])
+#         symbtrtxtURI = util.docserver_get_symbtrtxt(w['mbid'])
         
 # on other computer
-#         symbtrtxtURI = downloadSymbTr(w['mbid'], outputDir )
+        symbtrtxtURI = downloadSymbTr(w['mbid'], outputDir )
         
         if not symbtrtxtURI:
                 sys.exit("no symbTr found for work {}".format(w['mbid']) )
@@ -73,26 +79,52 @@ class LyricsAlign(compmusic.extractors.ExtractorModule):
 #                             lyrics_sim_thres=0.25, get_recording_rels=False)
         
         
-        sectionLinks = dunya.docserver.get_document_as_json(musicbrainzid, "scorealign", "sectionlinks", 1, version="0.2")
-
+     
+        
+        if WITH_SECTION_ANNOTATIONS:    
+            #### get section annotation file: TODO: create a makam/extractor instead 
+            sectionAnnosDict = get_section_annotaions_dict(musicbrainzid, outputDir) 
+            sectionLinksDict = sectionAnnosDict
+        else:
+            sectionLinksDict = dunya.docserver.get_document_as_json(musicbrainzid, "scorealign", "sectionlinks", 1, version="0.2")
+            
         extractedPitch = dunya.docserver.get_document_as_json(musicbrainzid, "initialmakampitch", "pitch", 1, version="0.6")
          
         
 #  on dunya server       
-        wavFileURI, created = util.docserver_get_wav_filename(musicbrainzid)
+#         wavFileURI, created = util.docserver_get_wav_filename(musicbrainzid)
 
 # on other computer         
-#         wavFileURI = download_wav(musicbrainzid, outputDir)
+        wavFileURI = download_wav(musicbrainzid, outputDir)
+
+
 
         wavFileURIMono = stereoToMono(wavFileURI)               
-        totalDetectedTokenList, sectionLinksDict = alignRecording(symbtrtxtURI, sectionMetadata, sectionLinks, wavFileURIMono, extractedPitch, outputDir)
+        totalDetectedTokenList, sectionLinksDict = alignRecording(symbtrtxtURI, sectionMetadata, sectionLinksDict, wavFileURIMono, extractedPitch, outputDir, WITH_SECTION_ANNOTATIONS)
 
         ret = {'alignedLyricsSyllables':{}, 'sectionlinks':{} }
         ret['alignedLyricsSyllables'] = totalDetectedTokenList
         ret['sectionlinks'] = sectionLinksDict
         return ret
 
-# if __name__=='__main__':
-#     la = LyricsAlign()
-#     la.run('727cff89-392f-4d15-926d-63b2697d7f3f','b')   
+
+def get_section_annotaions_dict( musicbrainzid, outputDir):
+        URL = 'https://raw.githubusercontent.com/georgid/turkish_makam_section_dataset/master/audio_metadata/' + musicbrainzid + '.json'
+        sectionAnnosURI = os.path.join(outputDir, musicbrainzid + '.json')
+        if not os.path.isfile(sectionAnnosURI):
+            print "fetching sections  annotation from URL {}...".format(URL)
+            fetchFileFromURL(URL, sectionAnnosURI)
+        with open(sectionAnnosURI) as f3:
+            sectionAnnosDict = json.load(f3) # use annotations instead of links
+        return sectionAnnosDict
+
+def fetchFileFromURL(URL, outputFileURI):
+        response = urllib2.urlopen(URL)
+        a = response.read()
+        with open(outputFileURI,'w') as f:
+            f.write(a)
+
+if __name__=='__main__':
+    la = LyricsAlign()
+    la.run('727cff89-392f-4d15-926d-63b2697d7f3f','b')   
         
