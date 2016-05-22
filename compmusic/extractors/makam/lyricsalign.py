f7749046aaaf7c129755617a0d269309944b2384
georgid <georgi.dzhambazov@upf.edu>
2016-05-22 16:51:57 +0200

    works with Lyricsaligner after refactoring

--- compmusic/extractors/makam/lyricsalign.py
+++ compmusic/extractors/makam/lyricsalign.py
@@ -23,13 +23,17 @@ from compmusic.dunya import makam
 import tempfile
 
 
-from align.LyricsAligner  import downloadSymbTr,  alignRecording, download_wav, stereoToMono
-# from align.MakamScore import loadMakamScore2
+from align.LyricsAligner  import downloadSymbTr,  LyricsAligner, download_wav, stereoToMono, loadMakamRecording
+from align.ParametersAlgo import ParametersAlgo
+ParametersAlgo.FOR_MAKAM = 1 
+
 
 dunya.set_token("69ed3d824c4c41f59f0bc853f696a7dd80707779")
 
 WITH_SECTION_ANNOTATIONS = 1
-
+PATH_TO_HCOPY= '/usr/local/bin/HCopy'
+# ANDRES. On kora.s.upf.edu
+# PATH_TO_HCOPY= '/srv/htkBuilt/bin/HCopy'
 
 class LyricsAlign(compmusic.extractors.ExtractorModule):
     _version = "0.1"
@@ -44,6 +48,10 @@ class LyricsAlign(compmusic.extractors.ExtractorModule):
 
 
 
+    def __init__(self, dataDir):
+        self.dataDir = dataDir
+
+
 
 
     def run(self, musicbrainzid, fname):
@@ -53,15 +61,7 @@ class LyricsAlign(compmusic.extractors.ExtractorModule):
             Sound and Music Computing Conference 2015.
             """
             
-        rec_data = dunya.makam.get_recording(musicbrainzid )
-        
-        if len(rec_data['works']) == 0:
-                raise Exception('No work on recording %s' % musicbrainzid)
-        
-        if len(rec_data['works']) > 1:
-                raise Exception('More than one work for recording %s Not implemented!' % musicbrainzid)
-        
-        w = rec_data['works'][0]
+        w = getWork(musicbrainzid)
         outputDir = tempfile.mkdtemp()
 
         # TODO: mark second verse symbTr second verse and get from a separate reposotory 
@@ -77,9 +77,9 @@ class LyricsAlign(compmusic.extractors.ExtractorModule):
 
         if WITH_SECTION_ANNOTATIONS:            #  becasue complying with  score metadata for symbTr1, on which annotations are done
             dir_ = 'scores/metadata/'
-            sectionMetadata = get_section_annotaions_dict(w['mbid'], dir_, outputDir)
+            sectionMetadataDict = get_section_annotaions_dict(w['mbid'], dir_, outputDir)
         else:
-            sectionMetadata = dunya.docserver.get_document_as_json(w['mbid'], "metadata", "metadata", 1, version="0.1")
+            sectionMetadataDict = dunya.docserver.get_document_as_json(w['mbid'], "metadata", "metadata", 1, version="0.1")
                       
         
      
@@ -108,14 +108,31 @@ class LyricsAlign(compmusic.extractors.ExtractorModule):
 
 
 
-        wavFileURIMono = stereoToMono(wavFileURI)               
-        totalDetectedTokenList, sectionLinksDict = alignRecording(symbtrtxtURI, sectionMetadata, sectionLinksDict, wavFileURIMono, extractedPitch, outputDir, WITH_SECTION_ANNOTATIONS)
+        wavFileURIMono = stereoToMono(wavFileURI)
+        if ParametersAlgo.WITH_ORACLE_ONSETS == 1:
+
+            fetchNoteOnsetFile(musicbrainzid, self.dataDir)
+        
+        recording = loadMakamRecording(musicbrainzid, wavFileURIMono, symbtrtxtURI, sectionMetadataDict, sectionLinksDict,  WITH_SECTION_ANNOTATIONS)
+        lyricsAligner = LyricsAligner(recording, WITH_SECTION_ANNOTATIONS, PATH_TO_HCOPY)
+    
+        totalDetectedTokenList, sectionLinksDict =  lyricsAligner.alignRecording( extractedPitch, outputDir)
+        
 
         ret = {'alignedLyricsSyllables':{}, 'sectionlinks':{} }
         ret['alignedLyricsSyllables'] = totalDetectedTokenList
         ret['sectionlinks'] = sectionLinksDict
         return ret
 
+
+def getWork( musicbrainzid):
+        rec_data = dunya.makam.get_recording(musicbrainzid)
+        if len(rec_data['works']) == 0:
+            raise Exception('No work on recording %s' % musicbrainzid)
+        if len(rec_data['works']) > 1:
+            raise Exception('More than one work for recording %s Not implemented!' % musicbrainzid)
+        w = rec_data['works'][0]
+        return w
 
 def get_section_annotaions_dict( musicbrainzid, dir_, outputDir):
         URL = 'https://raw.githubusercontent.com/georgid/turkish_makam_section_dataset/master/' + dir_ + musicbrainzid + '.json'
@@ -127,6 +144,30 @@ def get_section_annotaions_dict( musicbrainzid, dir_, outputDir):
             sectionAnnosDict = json.load(f3) # use annotations instead of links
         return sectionAnnosDict
 
+
+def fetchNoteOnsetFile(musicbrainzid,  datasetDir):
+    '''
+    fetch note onset annotations
+    '''
+    recIDoutputDir = os.path.join(datasetDir, musicbrainzid)         
+    if not os.path.isdir(recIDoutputDir):
+                os.mkdir(recIDoutputDir)
+             
+    onsetPath = os.path.join(recIDoutputDir,  musicbrainzid + '.alignedNotes.txt')
+    if not os.path.isfile(onsetPath):
+                work = getWork(musicbrainzid)
+                symbtr = dunya.makam.get_symbtr(work['mbid'])
+                symbTrCompositionName = symbtr['name']
+                URL = 'https://raw.githubusercontent.com/MTG/turkish_makam_audio_score_alignment_dataset/master/data/' + symbTrCompositionName + '/' + musicbrainzid + '/' + 'alignedNotes.txt' 
+                
+                # problem with symbTrComposition name
+                URL = 'https://raw.githubusercontent.com/MTG/turkish_makam_audio_score_alignment_dataset/master/data/nihavent--sarki--curcuna--kimseye_etmem--kemani_sarkis_efendi/feda89e3-a50d-4ff8-87d4-c1e531cc1233/alignedNotes.txt'
+                print "fetching notes score annotation from URL {}...".format(URL)
+                fetchFileFromURL(URL, onsetPath )
+                
+
+
+
 def fetchFileFromURL(URL, outputFileURI):
         response = urllib2.urlopen(URL)
         a = response.read()
@@ -134,6 +175,13 @@ def fetchFileFromURL(URL, outputFileURI):
             f.write(a)
 
 if __name__=='__main__':
-    la = LyricsAlign()
-    la.run('727cff89-392f-4d15-926d-63b2697d7f3f','b')   
+    
+#     if len(sys.argv) != 2:
+#         sys.exit('usage: {} <localpath>')
+#     la = LyricsAlign(sys.argv[1])
+    
+    la = LyricsAlign('/Users/joro/Downloads/ISTANBULSymbTr2/')
+#     la.run('727cff89-392f-4d15-926d-63b2697d7f3f','b')
+    la.run('567b6a3c-0f08-42f8-b844-e9affdc9d215','b')
+       
         
