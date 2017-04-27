f765030f3a3d97f10adceedb8a0859a343e0142c
Sertan Senturk <contact@sertansenturk.com>
2017-04-27 15:45:23 +0200

    Ensured all makam names are lowercase

--- compmusic/extractors/makam/scoresynthesis.py
+++ compmusic/extractors/makam/scoresynthesis.py
@@ -28,40 +28,40 @@ class ScoreSynthesis(compmusic.extractors.ExtractorModule):
         self.dataset = json.loads(response.read())
 
     def run(self, workid, fname):
-        try:
-            # get metadata
-            metadata = json.loads(compmusic.dunya.docserver.file_for_document(
-                workid, 'scoreanalysis', 'metadata'))
-            makam = metadata['makam']['attribute_key']
+        # try:
+        # get metadata
+        metadata = json.loads(compmusic.dunya.docserver.file_for_document(
+            workid, 'scoreanalysis', 'metadata'))
+        makam = metadata['makam']['attribute_key']
 
-            # fetch score
-            musicxml = compmusic.dunya.docserver.file_for_document(
-                recordingid=workid, thetype='score', subtype='xmlscore')
+        # fetch score
+        musicxml = compmusic.dunya.docserver.file_for_document(
+            recordingid=workid, thetype='score', subtype='xmlscore')
 
-            # read musicxml
-            (measures, makam, usul, form, time_sigs, keysig, work_title,
-             composer, lyricist, bpm, tnc_sym) = MusicXMLReader.read(musicxml)
+        # read musicxml
+        (measures, makam_dummy, usul, form, time_sigs, keysig, work_title,
+         composer, lyricist, bpm, tnc_sym) = MusicXMLReader.read(musicxml)
 
-            # synthesize according to AEU theory
-            self.set_settings()
-            audio_mp3, onsets = self.synth(bpm, measures, tnc_sym, None)
+        # synthesize according to AEU theory
+        self.set_settings()
+        audio_mp3, onsets = self.synth(bpm, measures, tnc_sym, None)
 
-            # check if it is in selected makams
-            if makam in self.dataset.keys():
-                recids = self.dataset[makam]
-                for recid in recids:
-                    stablenotes = json.loads(compmusic.dunya.file_for_document(
-                        recid, 'audioanalysis', 'note_models'))
+        # check if it is in selected makams
+        if makam in self.dataset.keys():
+            recids = self.dataset[makam]
+            for recid in recids:
+                stablenotes = json.loads(compmusic.dunya.file_for_document(
+                    recid, 'audioanalysis', 'note_models'))
 
-                    self.set_settings(recid)
+                self.set_settings(recid)
 
-                    audio_mp3, onsets = self.synth(
-                        bpm, measures, tnc_sym, stablenotes)
+                audio_mp3, onsets = self.synth(
+                    bpm, measures, tnc_sym, stablenotes)
 
-            return {workid: {'mp3': audio_mp3, 'onsets': onsets}}
+        return {workid: {'mp3': audio_mp3, 'onsets': onsets}}
 
-        except compmusic.dunya.conn.HTTPError:
-            print(workid, 'does not exist')
+        # except compmusic.dunya.conn.HTTPError:
+        #     print(workid, 'does not exist')
 
     def synth(self, bpm, measures, tnc_sym, stablenotes=None):
         # synthesize according to the given tuning
