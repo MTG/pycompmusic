7de62750c02247079eaecea3474ae0edc6e25553
Sertan Senturk <contact@sertansenturk.com>
2017-04-27 10:43:56 +0200

    Refactored scoresynthesis.py The synthesis operation was done outside the class as if it would be called as a script. These functionalities are moved to run() method. Note that the set_settings()is awry and the method does not return multiple audio per score yet. They will be taken care of in the next commits

--- compmusic/extractors/makam/scoresynthesis.py
+++ compmusic/extractors/makam/scoresynthesis.py
@@ -22,48 +22,12 @@ class ScoreSynthesis(compmusic.extractors.ExtractorModule):
                                            "mimetype": "application/json"}}
                         }
 
-    def set_parameters(self, measures, bpm, stablenotes, tnc_sym):
-        self.measures = measures
-        self.bpm = bpm
-        self.stablenotes = stablenotes
-        self.tnc_sym = tnc_sym
+        response = urllib2.urlopen(
+            'https://raw.githubusercontent.com/MTG/otmm_tuning_intonation_'
+            'dataset/master/dataset.json')
+        self.dataset = json.loads(response.read())
 
-    def run(self, musicbrainzid, fname):
-        try:
-            # synthesize
-            audio_karplus, onsets_karplus = AdaptiveSynthesizer.synth_from_tuning(
-                measures=self.measures, bpm=self.bpm,
-                stable_notes=self.stablenotes, tonic_sym=self.tnc_sym,
-                synth_type='karplus', verbose=True)
-
-            # mp3 conversion
-            audio_obj = pydub.AudioSegment(data=audio_karplus)
-            audio_mp3 = audio_obj.export()
-
-            return {musicbrainzid: {'mp3': audio_mp3,
-                                    'onsets': onsets_karplus}
-                    }
-
-        except compmusic.dunya.conn.HTTPError:
-            print(workid, 'is not exist')
-
-if __name__ == "__main__":
-    token = ''
-    compmusic.dunya.conn.set_token(token)
-
-    # fetch symbtrs
-    symbtrs = compmusic.dunya.get_collection('makam-symbtr')['documents']
-
-    response = urllib2.urlopen('https://raw.githubusercontent.com/MTG/otmm_tuning_intonation_dataset/master/dataset.json')
-    dataset = json.loads(response.read())
-    MAKAMS = dataset.keys()
-
-
-    synthesizer = ScoreSynthesis()
-
-    for xx, symbtr in enumerate(symbtrs):
-        workid = symbtr['external_identifier']
-
+    def run(self, workid, fname):
         try:
             # get metadata
             metadata = json.loads(compmusic.dunya.docserver.file_for_document(
@@ -78,25 +42,36 @@ if __name__ == "__main__":
             (measures, makam, usul, form, time_sigs, keysig, work_title,
              composer, lyricist, bpm, tnc_sym) = MusicXMLReader.read(musicxml)
 
+            # synthesize according to AEU theory
+            self.set_settings()
+            audio_mp3, onsets = self.synth(bpm, measures, tnc_sym, None)
+
             # check if it is in selected makams
-            if makam in MAKAMS:
-                recids = dataset[makam]
+            if makam in self.dataset.keys():
+                recids = self.dataset[makam]
                 for recid in recids:
                     stablenotes = json.loads(compmusic.dunya.file_for_document(
                         recid, 'audioanalysis', 'note_models'))
 
-                    synthesizer.set_settings(recid)
-                    synthesizer.set_parameters(measures=measures, bpm=bpm,
-                                               stablenotes=stablenotes,
-                                               tnc_sym=tnc_sym)
-                    print(xx, makam, recid)
-                    synthesizer.run(recid, None)
-            print('aeu', makam)
+                    self.set_settings(recid)
 
-            synthesizer.set_settings()
-            synthesizer.set_parameters(measures=measures, bpm=bpm,
-                                       stablenotes=None, tnc_sym=tnc_sym)
-            synthesizer.run('aeu', None)
+                    audio_mp3, onsets = self.synth(
+                        bpm, measures, tnc_sym, stablenotes)
+
+            return {workid: {'mp3': audio_mp3, 'onsets': onsets}}
 
         except compmusic.dunya.conn.HTTPError:
-            print(workid, 'is not exist')
+            print(workid, 'does not exist')
+
+    def synth(self, bpm, measures, tnc_sym, stablenotes=None):
+        # synthesize according to the given tuning
+        audio_karplus, onsets_karplus = AdaptiveSynthesizer. \
+            synth_from_tuning(measures=measures, bpm=bpm,
+                              stable_notes=stablenotes,
+                              tonic_sym=tnc_sym, synth_type='karplus',
+                              verbose=True)
+        # mp3 conversion
+        audio_obj = pydub.AudioSegment(data=audio_karplus)
+        audio_mp3 = audio_obj.export()
+
+        return audio_mp3, onsets_karplus
