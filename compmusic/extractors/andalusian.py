02d490f071dde154117f0884a08a768042ac89b9
Oriol Romani <oriol.romani@upf.edu>
2018-04-11 13:03:37 +0200

    Andalusian extractor. Extracts pitch and pitch distribution

---
+++ compmusic/extractors/andalusian.py
@@ -0,0 +1,33 @@
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+import numpy
+
+import compmusic.extractors
+
+from tomato.audio.predominantmelody import PredominantMelody
+from tomato.audio.pitchdistribution import PitchDistribution
+
+
+class Andalusian(compmusic.extractors.ExtractorModule):
+    _version = "0.1"
+    _sourcetype = "mp3"
+    _slug = "andalusian"
+    _output = {
+        "pitch": {"extension": "json", "mimetype": "application/json"},
+        "pitch_distribution": {"extension": "json", "mimetype": "application/json"},
+    }
+
+    def run(self, musicbrainzid, fname):
+        predominant_melody = PredominantMelody()
+
+        features = predominant_melody.extract(fname)
+
+        pitch = features.get('pitch', None)
+        if pitch:
+            pitch = list(numpy.array(pitch)[:, 1])
+
+        pitch_distribution = PitchDistribution.from_hz_pitch(pitch)
+        pitch_distribution.cent_to_hz()
+
+        return {"pitch": pitch, "pitch_distribution": pitch_distribution.vals}
