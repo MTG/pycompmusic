d8c8b143fdb9c7fafe45d787fa4cbd6a635273cc
Alastair Porter <alastair.porter@upf.edu>
2021-01-19 19:49:26 +0100

    Remove dunya-specific extractors and tools

--- compmusic/__init__.py
+++ compmusic/__init__.py
@@ -1,7 +1,3 @@
-from compmusic.musicbrainz import *
-from compmusic.file import *
-import compmusic.tags
-
 HINDUSTANI_COLLECTION="213347a9-e786-4297-8551-d61788c85c80"
 CARNATIC_COLLECTION="f96e7215-b2bd-4962-b8c9-2b40c17a1ec6"
 MAKAM_COLLECTION="544f7aec-dba6-440c-943f-103cf344efbb"
--- compmusic/extractors/makam/settings.example.py
+++
@@ -1,1 +0,0 @@
-token = "" # Dunya API Token
