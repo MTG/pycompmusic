6da37a4af40960a1cf7d718f7b22c6e94b84eee3
Alastair Porter <alastair.porter@upf.edu>
2024-10-10 10:11:27 +0200

    Update tool for package version

--- compmusic/__init__.py
+++ compmusic/__init__.py
@@ -8,6 +8,5 @@ MAKAM_COLLECTION="544f7aec-dba6-440c-943f-103cf344efbb"
 ANDALUSIAN_COLLECTION="142ea0d7-7fdf-4ea5-9b04-219f68023d01"
 BEIJING_COLLECTION="40d0978b-0796-4734-9fd4-2b3ebe0f664c"
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
