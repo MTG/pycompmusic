0ad127150c51c45249ce9fcfc5b83b951f13e3db
Alastair Porter <alastair.porter@upf.edu>
2024-10-31 16:01:53 +0100

    WIP: New setup tools

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
-
-from setuptools import setup, find_packages
-
+from setuptools import setup
 import versioneer
 
 setup(name='pycompmusic',
       version=versioneer.get_version(),
-      description='Tools for playing with the compmusic collection',
-      author='CompMusic / MTG UPF',
-      author_email='compmusic@upf.edu',
-      url='https://compmusic.upf.edu',
-      install_requires=['requests'],
-      packages=find_packages(),
       cmdclass=versioneer.get_cmdclass()
 )
